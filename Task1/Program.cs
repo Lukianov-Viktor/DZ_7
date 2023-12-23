de194fe135e67106ca2f70cc18f2339377c5101e
Lukianov Viktor <barkadaft@gmail.com>
2023-12-23 17:52:18 +0300

    add task 1

---
+++ Task1/Program.cs
@@ -0,0 +1,21 @@
+﻿// Задайте значения M и N. Напишите программу,
+// которая выведет все натуральные числа в промежутке от M до N.
+// Использовать рекурсию, не использовать циклы.
+
+using System.Reflection;
+
+Console.Write("Введите значение m: ");
+int m = Convert.ToInt32(Console.ReadLine());
+Console.Write("Введите значение n: ");
+int n = Convert.ToInt32(Console.ReadLine());
+void che(int m, int n)
+{
+    if (m > n)
+    return;
+    if (m % 2 == 0)
+    {
+        Console.Write($"{m},");
+    }
+    che(m + 1, n);
+}
+che(m,n);
